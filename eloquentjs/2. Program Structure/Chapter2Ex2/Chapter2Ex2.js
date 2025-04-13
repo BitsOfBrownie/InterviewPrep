e1cef20b7ad090e44ca781884bd4f312e55afd44
John Brown <j23b99@gmail.com>
2025-04-13 15:43:38 -0400

    Inital commit to fix folder path

---
+++ eloquentjs/2. Program Structure/Chapter2Ex2/Chapter2Ex2.js
@@ -0,0 +1,28 @@
+//cycles through designated number
+for (let i = 1; i <= 100; i++){
+
+    //checks if division remainder is 0
+    if (i % 5 === 0 && i % 3 === 0){
+
+        //prints appropriate message
+        console.log(`Fizz Buzz ${i}`);
+    }
+
+    //checks if division remainder is 0
+    else if (i % 5 === 0){
+
+        //prints appropriate message
+        console.log(`Buzz ${i}`);
+    }
+
+    //checks if division remainder is 0
+    else if (i % 3 === 0){
+
+        //prints appropriate message
+        console.log(`Fizz ${i}`);
+    }
+    else{
+        //prints appropriate non-Fizz non-Buzz number
+        console.log(i); 
+    }
+}
