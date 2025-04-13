55568f571faf6e1f29e70bc920d4a3881419dfbe
John Brown <j23b99@gmail.com>
2025-04-13 15:49:05 -0400

    folder rework

---
+++ eloquentjs/2. Program Structure/Chapter2Ex1/Chapter2Ex1.js
@@ -0,0 +1,13 @@
+//Establishes char variable for pyramid constructon
+let pyramidBuildingBlockChar = "";
+
+//itterates through desired size of pyramid
+for (let i = 0; i < 7; i++) 
+    {
+
+        //adds the char to corrispond with loop
+        pyramidBuildingBlockChar += "#";
+
+        //prints out char to build pyramid
+        console.log(pyramidBuildingBlockChar);
+    }
