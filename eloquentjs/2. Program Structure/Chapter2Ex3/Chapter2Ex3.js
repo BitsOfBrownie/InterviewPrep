0b013b1194db836fc6f9a0e9b5be34ab1df3bf9c
John Brown <j23b99@gmail.com>
2025-04-18 18:06:47 -0400

    Fixed Requested Changes 1

--- eloquentjs/2. Program Structure/Chapter2Ex3/Chapter2Ex3.js
+++ eloquentjs/2. Program Structure/Chapter2Ex3/Chapter2Ex3.js
@@ -1,32 +1,39 @@
 // Created variable for size
-let size = 8;
+let size = 73;
 
+// Keeps Track of Original Odd Status 
+let oddSizeTracker = false;
+if (size % 2 != 0) {
+    size++;
+    oddSizeTracker = true;
+}
 // Keeps track of when to alternate
-let count = size/2;
+let count = size / 2;
 let swap = true;
 
 // String to build board
-let stringForOutput = "";
+let chessBoardStructure = "";
 
 // Cycles through the half the size of the whole board of charactors length
-for (let i = 0; i < (size*size)/2; i++) {
+for (let i = 0; i < (size * size) / 2; i++) {
 
-    // Checks to see if it is time to swap patterns and add a break
+    // Checks to see if it is time to swap patterns, checks for/modifies "size" if odd, and adds a break
     if (i === count) {
-        stringForOutput += "\n";
-        count = count + size/2;
+        if (oddSizeTracker === true) {
+            chessBoardStructure = chessBoardStructure.slice(0,-1);
+        }
+        chessBoardStructure += "\n";
+        count += size / 2;
         swap = !swap;
     }
-
-    // Adds appropriate pattern
     if (swap) {
-        stringForOutput += " #";
+        // Adds appropriate pattern
+        chessBoardStructure += " #";
     }
-
-    // Adds appropriate pattern
     else {
-        stringForOutput += "# ";
+        // Adds appropriate pattern
+        chessBoardStructure += "# ";
     }
 }
 // Prints finished board
-console.log(stringForOutput);
+console.log(chessBoardStructure);
